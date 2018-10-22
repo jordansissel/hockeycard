3e84837d25a18f75df0fa6b8ac20f16fa2da6a80
Jordan Sissel <jls@semicomplete.com>
2018-10-21 23:36:14 -0700

    Small refactoring to make things a bit more readable. Exploring ways to get the full page HTML.

--- crawl.js
+++ crawl.js
@@ -1,44 +1,58 @@
 const HCCrawler = require('headless-chrome-crawler');
+const BaseCache = require('headless-chrome-crawler/cache/base');
 const URL = require("url").URL;
 
+class Cacher extends BaseCache {
+    // Cache into Elasticsearch
+}
+
+const HockeyCrawler = {
+    obeyRobotsTxt: false,
+
+    // Be kind to the remote webserver
+    maxConcurrency: 1,
+    delay: 5000,
+
+    // Number of clicks required to to view a scoresheet: 3
+    // Landing page -> team -> game scoresheet
+    maxDepth: 3,
+
+    // Don't need jQuery.
+    jQuery: false,
+
+    //cache: new Cacher(),
+
+    preRequest: (options) => {
+        const url = new URL(options.url);
+
+        // Only fetch pages that are relevant to finding game scoresheets
+        if (url.pathname === "/display-stats.php") {
+            return true;
+        }
+        if (url.pathname === "/display-schedule") {
+            return true;
+        }
+        if (url.pathname === "/oss-scoresheet") {
+            return true;
+        }
+        return false;
+    },
+
+    evaluatePage: () => ({
+        // Whole page as HTML text so that we can store it in Elasticsearch
+        html: new XMLSerializer().serializeToString(document),
+    }),
+    onSuccess: (result => {
+        console.log(result.result.html);
+        console.log("[" + result.result.html.length + " bytes] " + result.options.url)
+    }),
+    onError: (error => {
+        //console.log("Error", error);
+    }),
+};
+
 (async () => {
-    const crawler = await HCCrawler.launch({
-        // Be kind to the remote webserver
-        obeyRobotsTxt: false,
-        maxConcurrency: 1,
-        delay: 5000,
-
-        // Crawl somewhat deeply
-        // Number of visits in order to view a scoresheet: 3
-        // Landing page -> team -> game scoresheet
-        maxDepth: 3,
-
-        preRequest: (options) => {
-                const url = new URL(options.url);
-
-                // Only fetch pages that are relevant to finding game scoresheets
-                if (url.pathname === "/display-stats.php") {
-                    return true;
-                }
-                if (url.pathname === "/display-schedule") {
-                    return true;
-                }
-                if (url.pathname === "/oss-scoresheet") {
-                    return true;
-                }
-                return false;
-        },
-
-        evaluatePage: () => ({
-            title: $('title').text(),
-        }),
-        onSuccess: (result => {
-            console.log(result.options.url);
-        }),
-        onError: (error => {
-            //console.log("Error", error);
-        }),
-    });
+    const crawler = await HCCrawler.launch(HockeyCrawler);
     const entry = "https://stats.sharksice.timetoscore.com/display-stats.php?league=1&season=43";
     //crawler.queue('https://example.com/');
     crawler.queue("https://stats.sharksice.timetoscore.com/display-stats.php?league=1&season=43");
